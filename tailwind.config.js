ac0b0bbf423ad6b7dc3773599dd611e7fbf08c9f
Teneiei <tenmonsterten@gmail.com>
2024-09-30 14:39:36 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}", "./public/*.html", "./*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{html,js}", "./public/*.html", "./*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
